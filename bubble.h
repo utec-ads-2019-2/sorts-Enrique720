f2eed62a959b415a602b4a295dbcbe47b8ae5cb1
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-08-31 15:01:11 -0500

    Update bubble.h

--- bubble.h
+++ bubble.h
@@ -10,6 +10,11 @@ class BubbleSort : public Sort {
 
         void execute() {
             // TODO
+            int i, j;
+            for (i = 0; i < size-1; i++)
+                for (j = 0; j < size-i-1; j++)
+                    if (elements[j] > elements[j+1])
+                        swap(elements[j], elements[j+1]);
         }
 
         inline string name() { return "BubbleSort"; }
