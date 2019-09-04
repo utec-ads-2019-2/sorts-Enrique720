62510a3aa98062cd805a75f3c67bb82531abaaca
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-09-04 00:05:59 -0500

    Update radix.h

--- radix.h
+++ radix.h
@@ -6,10 +6,35 @@
 class RadixSort : public Sort {       
     public:
         RadixSort(int *elements, size_t size) : Sort(elements, size) {}
-
-        void execute() {
-            // TODO
+        int maximo(int n)
+    {
+        int max = elements[0];
+        for (int i = 1; i < n; i++)
+            if (elements[i] > max)
+                max = elements[i];
+            return max;
+    }
+    void countSort(int exp)
+    {
+        int salida[size];
+        int count[10] = {0};
+        for (int i = 0; i < size; i++)
+            count[ (elements[i]/exp)%10 ]++;
+        for (int i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+        for (int i = size - 1; i >= 0; i--)
+        {
+            salida[count[ (elements[i]/exp)%10 ] - 1] = elements[i];
+            count[ (elements[i]/exp)%10 ]--;
         }
+        for (int i = 0; i < size; i++)
+            elements[i] = salida[i];
+    }
+    void execute() {
+        int max = maximo(size);
+        for (int exp = 1; max/exp > 0; exp *= 10)
+            countSort(exp);
+    }
 
         inline string name() { return "RadixSort"; }
 };
