b69a2c6622526e43094484077a258bface72c189
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-09-04 00:00:32 -0500

    Update shell.h

--- shell.h
+++ shell.h
@@ -8,10 +8,18 @@ class ShellSort : public Sort {
         ShellSort(int *elements, size_t size) : Sort(elements, size) {}
 
         void execute() {
-            // TODO
+            for (int lim = size / 2; lim > 0; lim /= 2) {
+                for (int i = lim; i < size; i += 1) {
+                    int temp = elements[i];
+                    int j;
+                    for (j = i; j >= lim && elements[j - lim] > temp; j -= lim)
+                        elements[j] = elements[j - lim];
+                    elements[j] = temp;
+                }
+            }
         }
 
-        inline string name() { return "ShellSort"; }
+            inline string name() { return "ShellSort"; }
 };
 
 #endif
