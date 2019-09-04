d93d4eb8e30018d8c137dd156f82264f2243a4c5
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-09-03 23:59:23 -0500

    Update insert.h

--- insert.h
+++ insert.h
@@ -8,7 +8,19 @@ class InsertSort : public Sort {
         InsertSort(int *elements, size_t size) : Sort(elements, size) {}
 
         void execute() {
-            // TODO
+              int i, j;
+            int temp = 0;
+            for (i = 1; i < size; i++)
+            {
+                temp = elements[i];
+                j = i - 1;
+                while (j >= 0 && elements[j] > temp)
+                {
+                    elements[j + 1] = elements[j];
+                    j = j - 1;
+                }
+                elements[j + 1] = temp;
+            }
         }
 
         inline string name() { return "InsertSort"; }
