84e6026d270aa55f33906f65887f281f62c9adad
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-09-03 23:59:56 -0500

    Update quick.h

--- quick.h
+++ quick.h
@@ -6,12 +6,35 @@
 class QuickSort : public Sort {       
     public:
         QuickSort(int *elements, size_t size) : Sort(elements, size) {}
-
-        void execute() {
-            // TODO
+        int divison(int low, int high)
+         {
+        int pivote = elements[high];
+        int i = (low - 1);
+        for (int j = low; j <= high - 1; j++)
+        {
+            if (elements[j] < pivote)
+            {
+                i++;
+                swap(elements[i], elements[j]);
+            }
         }
+        swap(elements[i + 1], elements[high]);
+        return (i + 1);
+    }
+    void quickSort(int low, int high)
+    {
+        if (low < high)
+        {
 
-        inline string name() { return "QuickSort"; }
+            int p_index = divison(low, high);
+            quickSort(low, p_index - 1);
+            quickSort( p_index + 1, high);
+        }
+    }
+    void execute() {
+        quickSort(0, size - 1);
+    }
+    inline string name() { return "QuickSort"; }
 };
 
 #endif
